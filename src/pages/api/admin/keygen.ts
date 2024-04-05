df0eb4340548a1bbd8c27e7dcdd36c18a144e758
AndrewCLu <andrewclu98@gmail.com>
2024-04-05 16:00:54 -0400

    update keygen file with test data

--- src/pages/api/admin/keygen.ts
+++ src/pages/api/admin/keygen.ts
@@ -35,7 +35,8 @@ export default async function handler(
 
       // Logic for person chips
       if (chipData.type === "person") {
-        if (chipData.precreateUser) {
+        // If user is a speaker, precreate user object
+        if (chipData.speaker) {
           await prisma.user.create({
             data: {
               chipId,

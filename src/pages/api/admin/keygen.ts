9d1cc2bc3682baca3e657d49367aefccdadef5f5
AndrewCLu <andrewclu98@gmail.com>
2024-04-06 22:49:54 -0400

    add error

--- src/pages/api/admin/keygen.ts
+++ src/pages/api/admin/keygen.ts
@@ -177,6 +177,7 @@ export default async function handler(
 
     res.status(200).json({});
   } catch (error) {
-    res.status(500).json({ error: "Failed to read keygen file" });
+    console.log("Failed to generate keys", error);
+    res.status(500).json({ error: "Failed to generate keys" });
   }
 }
